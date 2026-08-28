a3219a267f256b4c7cf0f40a1b3433e2e222f719
usausa <machi.pon@gmail.com>
2026-08-29 08:09:10 +0900

    Update

--- BunnyTail.MemberAccessor.Generator/Diagnostics.cs
+++ BunnyTail.MemberAccessor.Generator/Diagnostics.cs
@@ -7,7 +7,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidTypeArgument { get; } = new(
         id: "BTMA0001",
         title: "Invalid type argument",
-        messageFormat: "Type must be generic type. type=[{0}]",
+        messageFormat: "Type must be a generic type. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -15,7 +15,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidAttributeLocation { get; } = new(
         id: "BTMA0002",
         title: "Invalid attribute location",
-        messageFormat: "Attribute must be in the same location as the target type. type=[{0}]",
+        messageFormat: "Attribute is in a different location. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -31,7 +31,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor TypedAccessorTargetNotDecorated { get; } = new(
         id: "BTMA0004",
         title: "TypedAccessor target not decorated",
-        messageFormat: "The target type of [TypedAccessor] does not have [GenerateAccessor]. type=[{0}]",
+        messageFormat: "Target type has no [GenerateAccessor]. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -39,7 +39,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor UnsupportedConstructorArity { get; } = new(
         id: "BTMA0005",
         title: "Unsupported constructor arity",
-        messageFormat: "Type has a constructor with more than {1} parameters, which is not supported. type=[{0}]",
+        messageFormat: "Constructor has more than {1} parameters. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -47,7 +47,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor TypeNotPartial { get; } = new(
         id: "BTMA0006",
         title: "Type is not partial",
-        messageFormat: "Type is not partial, so the IAccessorProvider implementation is not generated. type=[{0}]",
+        messageFormat: "IAccessorProvider is not generated. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Info,
         isEnabledByDefault: true);
@@ -55,7 +55,7 @@ internal static class Diagnostics
     public static DiagnosticDescriptor InvalidExternalTarget { get; } = new(
         id: "BTMA0007",
         title: "Invalid target type",
-        messageFormat: "The target type of [GenerateAccessorFor] is not supported. type=[{0}]",
+        messageFormat: "Target type is not supported. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -63,15 +63,15 @@ internal static class Diagnostics
     public static DiagnosticDescriptor AccessorAlreadyGenerated { get; } = new(
         id: "BTMA0008",
         title: "Accessor already generated",
-        messageFormat: "Accessor classes are already generated for the target type by [GenerateAccessor]. type=[{0}]",
+        messageFormat: "Target type already has [GenerateAccessor]. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Info,
         isEnabledByDefault: true);
 
     public static DiagnosticDescriptor GenericUnsafeAccessorNotSupported { get; } = new(
         id: "BTMA0009",
-        title: "Non-public member access on generic type requires .NET 9 or later",
-        messageFormat: "Non-public member access on a generic type uses UnsafeAccessor with generic parameters, which requires .NET 9 or later at runtime. type=[{0}]",
+        title: "Generic UnsafeAccessor not supported",
+        messageFormat: "Non-public member access needs .NET 9. type=[{0}]",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
